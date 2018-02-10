b1fbe2eb3292580c2706fb8a672929205835e9a9
Randy Knapp <randy.bravo2@gmail.com>
2018-02-10 13:37:14 -0800

    Loads the mod, does nothing else

---
+++ SubnauticaModSystem/MoreQuickSlots/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace MoreQuickSlots
+{
+    public class Controller : MonoBehaviour
+    {
+        private static readonly string GAME_OBJECT_NAME = "MoreQuickSlots.Controller";
+
+        private static ModLoader.ModEntry entry;
+
+        public string dir { get { return entry.path; } }
+
+        public static void Load(ModLoader.ModEntry obj = null)
+        {
+            Unload();
+            entry = obj;
+            new GameObject(GAME_OBJECT_NAME).AddComponent<MoreQuickSlots.Controller>();
+            Console.WriteLine("[MoreQuickSlots] Mod Loaded");
+        }
+
+        private static void Unload()
+        {
+            entry = null;
+            GameObject gameObject = GameObject.Find(GAME_OBJECT_NAME);
+            if (gameObject)
+            {
+                DestroyImmediate(gameObject);
+                Console.WriteLine("[MoreQuickSlots] Mod Unloaded");
+            }
+        }
+    }
+}
