1b5d7a87fe44d61fd0c98045d40b95117cc6a975
PrimeSonic <prime.sonic@gmail.com>
2020-05-16 16:53:47 -0400

    AutoSortSML - AssemblyInfo updates - Adding myself to mod.json for these efforts

--- SubnauticaModSystem/DockedVehicleStorageAccess/Properties/AssemblyInfo.cs
+++ SubnauticaModSystem/DockedVehicleStorageAccess/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MoreQuickSlots")]
+[assembly: AssemblyTitle("DockedVehicleStorageAccessSML")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MoreQuickSlots")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("DockedVehicleStorageAccessSML")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
